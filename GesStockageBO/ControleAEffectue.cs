f7b30fa38b67aa7b7ad2bd45f12a4787040a5a0d
SamiZ <sami.zerrai@gmail.com>
2019-10-09 17:09:32 +0200

    Ajout des couches ControlAEffectue

---
+++ GesStockageBO/ControleAEffectue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GesStockageBO
+{
+    class ControleAEffectue
+    {
+        private int id;
+        private DateTime dateControle;
+        private string commentaire;
+
+        public ControleAEffectue(int id, DateTime dateControle, string commentaire)
+        {
+            this.Id = id;
+            this.DateControle = dateControle;
+            this.Commentaire = commentaire;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public DateTime DateControle { get => dateControle; set => dateControle = value; }
+        public string Commentaire { get => commentaire; set => commentaire = value; }
+    }
+}
---
+++ GestionStockage/ControleAEffectueManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionStockage
+{
+    class ControleAEffectueManager
+    {
+        private static ControleAEffectueManager uneInstance;
+        public static ControleAEffectueManager GetInstance()
+        {
+            if (uneInstance == null)
+            {
+                uneInstance = new ControleAEffectueManager();
+            }
+            return uneInstance;
+        }
+    }
+}
